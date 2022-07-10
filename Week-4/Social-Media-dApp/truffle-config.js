a013e337f217f05c8bb471958e2f678e2094452c
pal-16 <mantrypalak@gmail.com>
2022-07-10 23:54:12 +0530

    added l2 polygon layer and code for subgraphs

--- Week-4/Social-Media-dApp/truffle-config.js
+++ Week-4/Social-Media-dApp/truffle-config.js
@@ -1,18 +1,25 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider=require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",    
+      port: 7545,            
+      network_id: "*",      
     },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.secret, process.env.url),
+      network_id: 80001,
+    }   
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+  
       optimizer: {
         enabled: true,
         runs: 200
